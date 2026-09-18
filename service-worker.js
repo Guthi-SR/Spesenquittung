49495867dc0f5f38a8ac31ac3437878c72fe39a4
Guthi-SR <daniel-14-05@web.de>
2026-09-18 11:48:08 +0200

    PWA-Cache auf v64 aktualisieren

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'spesenabrechnung-srg-msp-v63';
+const CACHE_NAME = 'spesenabrechnung-srg-msp-v64';
 const APP_SHELL = ['./','./index.html','./manifest.webmanifest','./icon.svg'];
 
 self.addEventListener('install', event => {
