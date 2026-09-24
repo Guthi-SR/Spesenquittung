3720ea01763ac7701e8a92a6feb6c15f335d210d
Guthi-SR <daniel-14-05@web.de>
2026-09-24 13:45:54 +0200

    v68: Web-App Cache aktualisieren

--- service-worker.js
+++ service-worker.js
@@ -1,47 +1,22 @@
-const CACHE_NAME = 'spesenabrechnung-srg-msp-v67';
-const APP_SHELL = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './logo-spesen.png'
-];
-
+const CACHE_NAME = 'spesenabrechnung-srg-msp-v68';
+const APP_SHELL = ['./','./index.html','./manifest.webmanifest','./logo-spesen.png'];
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(APP_SHELL))
-      .then(() => self.skipWaiting())
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
 });
-
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
-
 self.addEventListener('fetch', event => {
-  const req = event.request;
-  const url = new URL(req.url);
-
-  if (url.origin !== self.location.origin) return;
-
-  if (req.mode === 'navigate') {
-    event.respondWith(
-      fetch(req)
-        .then(resp => {
-          const copy = resp.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put('./index.html', copy));
-          return resp;
-        })
-        .catch(() => caches.match('./index.html'))
-    );
+  const req=event.request;
+  const url=new URL(req.url);
+  if(url.origin!==self.location.origin) return;
+  if(req.mode==='navigate'){
+    event.respondWith(fetch(req).then(resp=>{
+      const copy=resp.clone();
+      caches.open(CACHE_NAME).then(cache=>cache.put('./index.html',copy));
+      return resp;
+    }).catch(()=>caches.match('./index.html')));
     return;
   }
-
-  event.respondWith(
-    caches.match(req).then(cached => cached || fetch(req))
-  );
+  event.respondWith(caches.match(req).then(cached=>cached||fetch(req)));
 });
